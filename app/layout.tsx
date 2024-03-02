bfc9df86f1a446b7309a96c5d25bfb95bd7a8611
jaldip suthar <jaldipsuthar@gmail.com>
2024-03-02 14:14:47 +0530

    layout  making with nav

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "../styles/globals.scss";
+import Navbar from "@/components/navbar/page";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,6 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Navbar/>
       {children}
 
       </body>
